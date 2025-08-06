9c30453656deed5dde1a8a66c8e9fb1b68815644
Emerson <emerson.ramos.dos.santos2006@gmail.com>
2025-08-06 11:51:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,7 +95,12 @@ async function showDailyQuote() {
       dailyQuote = storedQuote.quote;
     } else {
       const randomIndex = Math.floor(Math.random() * quotes.length);
-      dailyQuote = quotes[randomIndex];
+      const randomQuote = quotes[randomIndex];
+
+      dailyQuote = {
+        message: randomQuote.quote,
+        author: randomQuote.author,
+      };
       localStorage.setItem(
         "dailyQuote",
         JSON.stringify({
@@ -105,7 +110,7 @@ async function showDailyQuote() {
       );
     }
 
-    dailyQuoteElement.textContent = `"${dailyQuote.message}"`;
+    dailyQuoteElement.innerHTML = `<i class="ph ph-quotes"></i> ${dailyQuote.message}`;
     dailyAuthorElement.textContent = `- ${dailyQuote.author}`;
   } catch (error) {
     console.error("Error", error);
